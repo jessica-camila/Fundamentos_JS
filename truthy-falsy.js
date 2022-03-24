b08ddfff63f90fd87f68898616dd5188c87ffb9b
Jessica Bueno <jessica-camila@sicredi.com.br>
2022-03-24 15:32:21 -0300

    variaveis

---
+++ truthy-falsy.js
@@ -0,0 +1,21 @@
+// boolean
+const usuarioLogado = true;
+const contapaga = false;
+
+// truthy ou falsy
+
+// 0 => false
+// 1 => true
+
+// console.log(0 == false)
+// console.log("" == false)
+
+// null ==> vazio ou nada
+
+// undefined
+
+let minhaVar;
+//let varNull = null;
+
+console.log(minhaVar)
+//console.log(varNull)
