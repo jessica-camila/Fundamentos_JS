c66983e9554600ade6be59ecd7b9c1b0bf462a21
jessica_camila <jessica_camila@sicredi.com.br>
2022-12-26 20:14:07 -0300

    teste

---
+++ JavaScript/Teste.js
@@ -0,0 +1,6 @@
+//console.log('Olá, mundo!');
+
+x = [2,3,4];
+
+console.log(x[2]);
+
---
+++ script.js
@@ -0,0 +1,11 @@
+// selecionar elemento
+let title = document.querySelector('#title');
+
+console.log(title);
+
+// innerHTML
+title.innerHTML = "Testando o texto alterado!";
+
+
+
+// textContent -> mais utilizado, recomendado 
